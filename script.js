4a20eb2e30563ad86829ab6bc79350dd1a4ca627
James Monroe <jay@jamesmonroedesign.com>
2022-08-23 23:19:13 -0500

    js event listener open

---
+++ script.js
@@ -0,0 +1,8 @@
+
+const menuBtn = document.querySelector('.menu-btn')
+    backBtn = document.querySelector('.back-btn')
+    menu = document.querySelector('nav');
+
+menuBtn.addEventListener('click', () => {
+    menu.style.transform = 'translateX(0)'
+})
